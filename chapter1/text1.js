e6cf2ba0eec84eb66c5df1cdd6ec8b737a4c7a5c
Phway <ayethandarphwaye@gmail.com>
2024-06-28 20:49:17 +0630

    first commit

---
+++ chapter1/text1.js
@@ -0,0 +1,10 @@
+// function sayhi(){
+//     alert("Win Min Hein")
+// }
+var x=window.prompt("Please enter your name:");
+   if(x)
+    window.alert("My name is"+x);
+
+else{
+    window.alert("Win Min Hein");
+}
---
+++ chapter1/text2.js
@@ -0,0 +1,7 @@
+var x=window.prompt("Please enter your name:");
+   if(x)
+    window.alert("My name is"+x);
+
+else{
+    window.alert("Nameless");
+}
